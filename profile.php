b06516d570870ea776b6573f00f4c87f68d3479a
Ryan Mraz <rmm0049@mix.wvu.edu>
2020-09-30 21:29:37 -0400

    lab5

--- profile.php
+++ profile.php
@@ -1,13 +1,83 @@
 <?php 
-require 'includes/header.php'
+require 'includes/header.php';
+require 'includes/dbhandler.php';
 ?>
 
 <main>
 
 <?php
 if (isset($_SESSION['uid'])){
-    include 'html/profile.html';
-}else{
+    $prof_user = $_SESSION['username'];
+    $sqlpro = "SELECT * FROM profile WHERE uname='$prof_user';";
+
+    $result = mysqli_query($conn, $sqlpro);
+    $row = mysqli_fetch_array($result);
+    $photo = $row['picpath'];
+?>
+<style>
+  .center-me {
+      display: flex;
+      justify-content: center;
+      padding: 40px;
+      text-align: "center";
+  }
+  #prof-display {
+      display: block;
+      
+      width: 150px;
+      margin: 10px auto;
+      border-radius: 50%;
+  }
+  
+  #uname-style {
+      font-size: 20px;
+      font-family: "Lucida Console", Courier, monospace;
+      font-weight: bold;
+  }
+  
+  </style>
+  
+  <script>
+      function triggered(){
+          document.querySelector("#prof-image").click();
+      }
+  
+      function preview(e){
+          if(e.files[0]){
+              var reader = new FileReader();
+  
+              reader.onload = function(e){
+                  document.querySelector('#prof-display').setAttribute('src',e.target.result);
+              }
+              reader.readAsDataURL(e.files[0]);
+  
+          }
+      }
+  </script>
+
+<html>
+  <body>
+    <div class="h-100 center-me text-center">
+      <form action="../includes/upload-helper.php" method="POST" enctype="multipart/form-data">
+        <div class="form-group">
+          <img src="<?php echo $photo?>" onclick="triggered();" id="prof-display">
+          <label for="prof-image" id="uname-style"><?php echo $prof_user?></label>
+          <input type="file" name="prof-image" id="prof-image" onchange="preview(this);" class="form-control" style="display: none;">
+        </div>
+        <div class="form-group">
+          <textarea name="bio" id="bio" cols="30" rows="10" placeholder="Bio..." style="text-align: center;"></textarea> </textarea>
+        </div>
+        <div class="form-group">
+          <button type="submit" name="prof-submit" class="btn-outline-success btn-lg btn-block">Upload</button>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
+
+<?php
+ }
+else{
     header("Location: login.php");
     exit();
 }
