0de28c644b2a63329df757a62c75477b7dfd43b3
Ryan Mraz <rmm0049@mix.wvu.edu>
2020-10-30 18:52:16 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+require "includes/header.php"
+?>
+
+
+<main>
+  <link rel="stylesheet" type="text/css" href="css/about.css" />
+  <title>About</title>
+  <div class="about">
+    <center>
+      <h1>About Us</h1>
+      <img class="logo" src="images/meallife.png" width="175" height="175" />
+      <h3>
+        <p>Meet Our Founders!</p>
+      </h3>
+      <h5 class="company">
+        Here at Meal Life we strive to provide a seamless meal prep service
+        that meets all of your needs all in one place. We value quality, fresh
+        ingredients that are sourced locally all over the country and strive
+        to make it an affordable option for families to enjoy.
+      </h5>
+    </center>
+  </div>
+  <h2 class="h2-header">Founding Members and Executives</h2>
+  <div class="row">
+    <div class="content">
+      <h3 class="content-title">Founder and CEO</h3>
+      <p class="content-name">
+        Darius Williams
+      </p>
+      <p class="content-paragraph">
+        Darius is graduate of Ohio State University in Columbus, OH where he
+        posses a Bachelor of Science degree in Nurtition and Exercise Science.
+        He used this background to work as a personal trainer as well as a
+        dietitian to help out clients. He founded Meal Life in 2015 when he
+        saw that even with strong nutrition advice, his clients still
+        struggled to adhere to it because of their busy lives and so he sought
+        to create a service that makes eating clean and healthy simple.
+      </p>
+      <img class="content-img" src="images/person1.jpg" width="175" height="125" />
+      <button type="button" class="btn btn-info">More About</button>
+    </div>
+    <div class="content">
+      <h3 class="content-title">Co-founder and Vice President</h3>
+      <p class="content-name">
+        Mark Johnson
+      </p>
+      <p class="content-paragraph">
+        Mark with his background in business, partnered with Darius to put the
+        idea of Meal Life into action and create a functinal business model
+        around it. He has successfully started and ran three businesses in the
+        past and brings his expertise to the team as acting vice president.
+      </p>
+      <img class="content-img" src="images/person2.jpg" width="175" height="125" />
+      <button type="button" class="btn btn-info">More About</button>
+    </div>
+    <div class="content">
+      <h3 class="content-title">CFO</h3>
+      <p class="content-name">
+        Brad Smith
+      </p>
+      <p class="content-paragraph">
+        Brad is the acting Chief Financial Officer of Meal Life who runs all
+        of the company's financial decisions and oversees financial
+        departments. He has a Bachelor of Science degree in Economics from
+        University of Chicago as well as a MBA.
+      </p>
+      <img class="content-img" src="images/person3.jpg" width="175" height="125" />
+      <button type="button" class="btn btn-info">More About</button>
+    </div>
+    <div class="content">
+      <h3 class="content-title">COO</h3>
+      <p class="content-name">
+        David Brown
+      </p>
+      <p class="content-paragraph">
+        David is Meal Life's Chief Operating Officer and oversees the day to
+        day administrative functions at the company. He has previous
+        experience in the meal prep industry where he brings over a decade of
+        experience with him.
+      </p>
+      <img class="content-img" src="images/person4.jpg" width="175" height="125" />
+      <button type="button" class="btn btn-info">More About</button>
+    </div>
+  </div>
+</main>
